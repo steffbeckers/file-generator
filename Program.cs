3215f31cf20abd31f85f6f606ee01c06ca5feb2c
Steff Beckers <s.beckers@codefined.be>
2023-09-10 20:43:17 +0200

    WIP file generator PDF

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+Command rootCommand = new RootCommand("File generator");
+
+Option<string> fileNameOption = new Option<string>("--name", "File name");
+rootCommand.AddGlobalOption(fileNameOption);
+
+rootCommand.SetHandler((context) =>
+{
+	Console.WriteLine("Hello World!");
+});
+
+Command pdfCommand = new Command("pdf", "Generate PDF file");
+
+Option<string> fromHtmlOption = new Option<string>("--from-html", "Generate PDF from HTML");
+pdfCommand.AddOption(fromHtmlOption);
+
+pdfCommand.SetHandler((context) =>
+{
+	string? fileName = context.ParseResult.GetValueForOption(fileNameOption);
+	string? fromHtml = context.ParseResult.GetValueForOption(fromHtmlOption);
+
+	Console.WriteLine($"Generating PDF file with name {fileName} from HTML {fromHtml}");
+});
+
+rootCommand.AddCommand(pdfCommand);
+
+await rootCommand.InvokeAsync(args);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # file-generator
+
+```
+dotnet pack -c Release
+dotnet tool install --global --add-source ./nupkg FileGenerator --version 0.1.0
+file-generator
+```
